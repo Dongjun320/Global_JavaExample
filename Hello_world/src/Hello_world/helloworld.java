fade162df670c29df1d0650cb38878294888dd7d
Dongjun320 <155514648+Dongjun320@users.noreply.github.com>
2026-04-27 11:59:41 +0900

    두번째 커밋사항

--- Hello_world/src/Hello_world/helloworld.java
+++ Hello_world/src/Hello_world/helloworld.java
@@ -4,7 +4,13 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		
+		int i = 10;
+		
+		i++;
+		
+		System.out.println(args[0]);
+		
 		System.out.println("Hello world");
+		
 	}
-
 }
