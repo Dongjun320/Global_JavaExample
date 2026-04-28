7e19aaf35eb33e8ff9e0dd54941defdb9e552215
Dongjun320 <155514648+Dongjun320@users.noreply.github.com>
2026-04-28 10:56:56 +0900

    > commit example

---
+++ TEST/src/Hello_world/helloworld.java
@@ -0,0 +1,16 @@
+package Hello_world;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		
+		int i = 20;
+		
+		i++;
+		
+		System.out.println(i);
+		
+		System.out.println("Hello world");
+		
+	}
+}
