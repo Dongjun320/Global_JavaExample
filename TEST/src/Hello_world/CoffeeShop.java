cd43420ce0fe611458e611835acc6f25a983ba51
Dongjun320 <ehdwns010320@naver.com>
2026-05-06 10:57:12 +0900

    객체지향테스트

---
+++ TEST/src/Hello_world/CoffeeShop.java
@@ -0,0 +1,11 @@
+package Hello_world;
+
+public class CoffeeShop {
+	
+	//회원가입 기능
+	void joinMember(exampleOOP exampleOOP) {
+		//user ID 발급받기
+		exampleOOP.name = "777";
+		System.out.println();
+	}
+}
---
+++ TEST/src/Hello_world/exampleOOP.java
@@ -0,0 +1,14 @@
+package Hello_world;
+
+public class exampleOOP {
+	
+	//정보, 속성 information, properties, atribute,
+	//char[] name;
+	String name;
+	
+	//처리, 함수, 기능 method, funtion, action
+	void readUserId() {
+		
+	}
+			
+}
